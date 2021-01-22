3ccf65ebd621eedfaf16aab3757f185e20ebfed5
unknown <tripptofan@gmail.com>
2021-01-22 15:53:02 -0500

    initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All Authors Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+             searchOptions: req.query 
+            })
+    } catch (error) {
+        res.redirect('/')
+    }
+})
+
+//New Author Route
+router.get('/new', async (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+
+//Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+    } catch (err) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+           // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    }
+})
+
+
+module.exports = router;
